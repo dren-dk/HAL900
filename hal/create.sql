8dc28284ff8abf8f78356b0a792197d9189537ed
ff <ff@a092026d-3e22-4cff-94a7-eaa6fdd35d37>
2010-05-13 12:09:58 +0000

    More work on the datamodel
    git-svn-id: file:///home/ff/projects/osaa/svn@2 a092026d-3e22-4cff-94a7-eaa6fdd35d37

--- hal/create.sql
+++ hal/create.sql
@@ -6,6 +6,8 @@
  ... or pg_restore -d hal -U hal -c latest.pg 
 */
 
+begin transaction;
+
 create table memberType (
        id integer primary key, 	
        created timestamp default now(),
@@ -34,38 +36,62 @@ create table member (
        smail varchar(150),
 
        doorAccess boolean,  
-       adminAccess boolean, /* Admin access via the web interface */
+       adminAccess boolean /* Admin access via the web interface */
 );
+
+create table accountType (
+       id integer primary key,
+       created timestamp default now(),
+       typeName varchar(50),
+);
+insert into accountType (id, typeName) values (1, 'Main organizational account');
+insert into accountType (id, typeName) values (2, 'Personal dues and purchases');
+insert into accountType (id, typeName) values (3, 'Loans');
+
+create table account (
+       id serial primary key,
+       created timestamp default now(),
+       owner_id integer references member(id),
+       type_id integer references accountType(id) not null,
+       
+       accountName varchar(50)
+);
+insert into account (type_id, accountName) values (1, 'OSAA kassebeholdning');
+
+create table accountTransaction (
+       id serial primary key,
+       created timestamp default now(),
+
+       source_account_id integer not null references account(id),
+       target_account_id integer not null references account(id),
+       check (source_account_id <> target_account_id),
+
+       amount numeric(10,2) check (amount > 0),
+
+       comment varchar(50),
+}
+
 
 create table bankBatch (
        id serial primary key,
        created timestamp default now(),
        member_id integer references member(id),
 
-       rawCsv varchar,       
+       rawCsv varchar,
 );
 
-
-create table payment (
+create table bankTransaction (
        id serial primary key,
        created timestamp default now(),
        member_id integer references member(id),
-       bankBatch_id integer references bankBatch(id),
+       bankBatch_id integer references bankBatch(id) not null,
        
        bankDate date,
        bankComment varchar(50),
        amount numeric(10,2),
-       
+
+       transaction_id integer references accountTransaction(id),
        userComment varchar(100), /* Info from the administrators about it */
-
-       appoved timestamp, /* Timestamp of the approval of this */
 );
 
-create table membershipFee (
-       id serial primary key,
-       created timestamp default now(),
-       member_id integer references member(id),
-
-       amount numeric(10,2),
-);
-
+commit;

74f5243565b4f70a23619d30f15e46f253282ecc
ff <ff@a092026d-3e22-4cff-94a7-eaa6fdd35d37>
2010-05-15 18:08:09 +0000

    Gah
    git-svn-id: file:///home/ff/projects/osaa/svn@8 a092026d-3e22-4cff-94a7-eaa6fdd35d37

--- hal/pm/HAL/Page/Front.pm
+++ hal/pm/HAL/Page/Front.pm
@@ -14,7 +14,11 @@ sub outputFrontPage($$$;$) {
 	    name=>'index',
 	    title=>'HAL:900',
 	},
-
+	{
+	    link=>"/hal/new",
+	    name=>'new',
+	    title=>'Ny bruger',
+	},
 
 	);
 
@@ -72,11 +76,37 @@ sub noCookie {
       "<p>You seem to have turned off cookie support, fix it KTHXBAI.</p>");
 }
 
+sub textInput {
+    my ($title, $lead, $name, $p) = @_;
+
+    my $v = $p->{$name} || '';
+    my $e = entity_encode($v);
+    return qq'
+<h4>$title</h4>
+<p class="lead">$lead</p>
+<input type="text" name="$name" size="50" value="$e">
+';
+}
+
+sub newUser {
+    my ($r,$q,$p) = @_;
+    
+    
+
+    my $form = qq'
+<form method="POST" target="/hal/new">
+<input type="submit" value="Videre">
+</form>
+    ';
+
+    return outputFrontPage("new", "Opret nyt medlemsskab", $form);
+}
 
 
 BEGIN {
     addHandler(qr'^/hal/?$', \&index);
     addHandler(qr'^/hal/nocookie$', \&noCookie);
+    addHandler(qr'^/hal/new$', \&newUser);
     addHandler(qr'^/', \&notFound, 10000);
 }
 

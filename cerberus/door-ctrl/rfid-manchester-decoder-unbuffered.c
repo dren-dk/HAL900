3f72b40ba9133e4e21dab97d842a113a122af50f
ff <ff@a092026d-3e22-4cff-94a7-eaa6fdd35d37>
2010-10-03 20:26:04 +0000

    Prettied up the unbuffered rfid decoder
    git-svn-id: file:///home/ff/projects/osaa/svn@143 a092026d-3e22-4cff-94a7-eaa6fdd35d37

--- cerberus/door-ctrl/rfid-manchester-decoder-unbuffered.c
+++ cerberus/door-ctrl/rfid-manchester-decoder-unbuffered.c
@@ -4,31 +4,45 @@
 #include <string.h>
 
 /*
-  This function eats edges from the signal.
-  * Positive numbers are rising edges
-  * Negative numbers are falling edges
-  * The size of the numbers is the length of time since the last edge.
+  This file implements an efficient, almost bufferles RFID Manchester decoder.
+
+  You feed it the signal obtained from the ICP interrupt and any detected RFID
+  codes become available via the getCurrentRfid and getLastRfid functions.
 */
 
 unsigned char headerLength = 0; // The number of short header bits seen.
-char rfidInUse = 0;            // -1 = Looking for the header.
+char rfidInUse = 0;             // -1 = Looking for the header.
 unsigned char rfid[7];          // Temporary storage for the datagram.
-#define PUSH_ONE  { rfid[rfidInUse>>3] |=  (1<<(rfidInUse & 7)); rfidInUse++; }
-#define PUSH_ZERO { rfidInUse++; }
-#define GET_BIT(x)  (rfid[(x)>>3] & (1<<((x) & 7)))   
-char halfBit = 0;
+char halfBit = 0; // Are we in the middle of a bit?
 
-#define RFID_RESET {rfidInUse=0; headerLength=0; memset(rfid, 0, 7); halfBit=0; }
+void pushOne() {
+  rfid[rfidInUse>>3] |= 1<<(rfidInUse & 7);
+  rfidInUse++;
+}
 
-void addEdge(char length) {
-  
-  //  fprintf(stdout, " %d", length);
+void pushZero() {
+  rfidInUse++;
+}
 
-  char edge = 0;
-  if (length < -76) edge--;
-  if (length < -10) edge--;
-  if (length > 76) edge++;
-  if (length > 10) edge++;
+char getBit(char x) {
+  return rfid[x>>3] & (1<<(x&7));
+}
+
+void resetRfidState() {
+  headerLength=0;
+  rfidInUse=0;
+  memset(rfid, 0, 7);
+  halfBit=0;
+}
+
+/*
+  This function eats edges from the signal, each edge is encoded as one of:
+  * -2: A long low-period, ending in a rising edge.
+  * -1: A short low-period, ending in a rising edge.
+  * +1: A short high-period, ending in a falling edge.
+  * +2: A long high-period, ending in a falling edge.
+*/
+void addEdge(char edge) {
 
   fprintf(stdout, " %d", edge);
 
@@ -59,26 +73,26 @@ void addEdge(char length) {
       fprintf(stdout, "\nD");
     }
 
-    if (edge > 1) {  // Long low period, rising edge => It's a one
+    if (edge > 1) {
       
       if (halfBit) {
 	fprintf(stdout, " <--- Manchester violation\n");
-	RFID_RESET
+	resetRfidState();
 
       } else {
-	PUSH_ZERO;
+	pushZero();
 	fprintf(stdout, "L");
 	halfBit = 0;
       }
 
-    } else if (edge < -1) { // Long high period, falling edge => It's a zero 
+    } else if (edge < -1) {
 
       if (halfBit) {
 	fprintf(stdout, " <--- Manchester violation\n");
-	RFID_RESET
+	resetRfidState();
 	
       } else {
-	PUSH_ONE;
+	pushOne();
 	fprintf(stdout, "l");
 	halfBit = 0;
       }
@@ -88,7 +102,7 @@ void addEdge(char length) {
       if (halfBit) {
 	halfBit = 0;
 	fprintf(stdout, "S");
-	PUSH_ZERO;
+	pushZero();
       } else {
 	halfBit = edge;
 	fprintf(stdout, "½");	
@@ -98,7 +112,7 @@ void addEdge(char length) {
       if (halfBit) {
 	halfBit = 0;
 	fprintf(stdout, "s");
-	PUSH_ONE;
+	pushOne();
       } else {
 	halfBit = edge;
 	fprintf(stdout, "½");	
@@ -106,11 +120,11 @@ void addEdge(char length) {
     }
   }
 
-  // Detect that we are done reading, then check parity and stopbits and parse out to output datatypes.
+  // Detect that we are done reading, then check parity and stopbits and parse out to output data.
   if (rfidInUse == 5*8+15) { // 5 bytes of data + 15 bits of parity.
     fprintf(stdout, "\nCaptured datagram:\n"); 
     for (char i=0;i<rfidInUse;i++) {
-      if (GET_BIT(i)) {
+      if (getBit(i)) {
 	fprintf(stdout, "1");
       } else {
 	fprintf(stdout, "0");
@@ -126,7 +140,7 @@ void addEdge(char length) {
       char rowParity = 0;
       for (unsigned char col=0;col<5;col++) {
 	char bit = row*5+col;
-	if (GET_BIT(bit)) {
+	if (getBit(bit)) {
 	  rowParity ^= 1;
 	  colParity ^= 16>>col;
 	}	
@@ -147,14 +161,14 @@ void addEdge(char length) {
       }
     }
 
-    if (GET_BIT(5*8+14)) {
+    if (getBit(5*8+14)) {
       fprintf(stdout, "Stop bit: Bad\n");	
     } else {	
       fprintf(stdout, "Stop bit: Ok\n");
     }    
 
     // Ready to go again...
-    RFID_RESET
+    resetRfidState();
   }
 }
 
@@ -171,9 +185,16 @@ int main(int argc, char **argv) {
       buffy[strlen(buffy)] = ch;
 
     } else if (strlen(buffy) > 0) {
-      int i = atoi(buffy);
+      int length = atoi(buffy);
       memset(buffy, 0, 10);
-      addEdge(i);
+
+      char edge = 0;
+      if (length < -76) edge--;
+      if (length < -10) edge--;
+      if (length > 76) edge++;
+      if (length > 10) edge++;
+
+      addEdge(edge);
     }
   }
   fclose(f);

1006aededd1fd7b141b1207938b5391fa24d0357
Flemming Frandsen <dren.dk@gmail.com>
2011-02-09 18:57:48 +0100

    Moved the secret email salt value out of the code and into a separate config file

--- hal/config/bootstrap-ff.pl
+++ hal/config/bootstrap-ff.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use lib "/home/ff/projects/osaa/hal/pm";
+use lib "/home/ff/projects/osaa/HAL900/hal/pm";
 use HAL;
 use HAL::UI;
 
--- hal/pm/HAL.pm
+++ hal/pm/HAL.pm
@@ -32,27 +32,48 @@ sub configureHAL {
 
     my %CONFIG = (
 	panther=>{
-	    root=>"/home/ff/projects/osaa/hal",
+	    root=>"/home/ff/projects/osaa/HAL900/hal",
 	    test=>1,
-	    salt=>'345klj56kl',
 	    db=>'dbi:Pg:dbname=hal;port=5433',
 	},
 	hal=>{
 	    root=>"/home/hal/hal",
 	    test=>0,
-	    salt=>'345klj56kl',
 	    db=>'dbi:Pg:dbname=hal;port=5432'	    
 	},
 	lisbeth=>{
 	    root=>"/home/jacob/Desktop/HACK/hal",
 	    test=>1,
-	    salt=>'345klj56kl',
 	    db=>'dbi:Pg:dbname=hal;port=5432',
 	},
 
     );
     
     $config = $CONFIG{$host} || die "HOST=$host is not found in \%CONFIG, cannot self-configure, fix pm/HAL.pm";
+
+    my $sf = "$config->{root}/.hal/web-salt.txt";
+    unless (-f $sf) {
+	mkdir "$config->{root}/.hal";
+
+	my $k = '';
+	my $sep = '';
+	open R, "</dev/urandom" or die "Fail: $!";
+	for my $i (0..31) {
+	    my $data;
+	    read(R, $data, 1) or die "Couldn't read from /dev/urandom";
+	    my $byte = unpack('C',$data);
+	    $k .= sprintf("%0x", $byte);
+	}
+	close R;
+
+	open K, ">$sf" or die "Failed to wite $sf: $!";
+	print K $k;
+	close K;
+    }
+
+    open K, "<$sf" or die "Failed to read $sf: $!";
+    $config->{salt} = join '', <K>;
+    close K;
 }
 
 BEGIN {
